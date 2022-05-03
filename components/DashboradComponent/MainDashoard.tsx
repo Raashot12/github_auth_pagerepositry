8546967b0050679e9f758cde74313a5177955da8
Raashot12 <akannirasheed1996@gmail.com>
2022-05-03 18:10:33 +0100

    done

--- components/DashboradComponent/MainDashoard.tsx
+++ components/DashboradComponent/MainDashoard.tsx
@@ -9,7 +9,8 @@ type UserProject = {
   id: number,
   name: string,
   url: string,
-  description:string
+  description: string
+  language:string
 }
 const MainDashoard = () => {
   const boardResult = useSelector((state: RootState) => state.details)
@@ -29,14 +30,15 @@ const MainDashoard = () => {
                 return (
                   <main className="repository-card-container" key={data?.id}>
                     <div>
-                      <Link
-                        href={`https://github.com/Raashot12/${data.name}`}
-                      >
+                      <Link href={`https://github.com/Raashot12/${data.name}`}>
                         <a className="project-name">{data.name}</a>
                       </Link>
                       <p className="description">{data.description}</p>
                     </div>
-                    <div>Hello</div>
+                    <div>
+                      <span></span>
+                      <p className="programming-language">{data.language}</p>
+                    </div>
                   </main>
                 )
               })}
--- components/DashboradComponent/Sidebar.tsx
+++ components/DashboradComponent/Sidebar.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable @next/next/link-passhref */
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react"
-import {signIn, useSession, signOut} from "next-auth/react"
 import {useSelector} from "react-redux"
 import {RootState} from "./store"
 
@@ -11,10 +10,7 @@ type Name = {
   sidebarData: any
 }
 const Sidebar = () => {
-  const {data, status} = useSession()
-
   const sidebarResult: Name = useSelector((state: RootState) => state.sidebar)
-
   return (
     <div className="sidebar-parent">
       {sidebarResult.loading === "pending" ? (
@@ -139,30 +135,3 @@ const Sidebar = () => {
 
 export default Sidebar
 
-{
-  /* {status === "authenticated" ? (
-        <>
-          <div>
-            {data.user?.image && data.user.name ? (
-              <div>
-                <img
-                  src={data.user.image}
-                  alt={data.user.name}
-                  width={260}
-                  height={260}
-                  style={{borderRadius: "50%", margin: "0 auto"}}
-                />
-                <div>
-                  <h1 style={{color: "#181819", fontSize: "24px", marginBottom:"4px"}}>
-                    {data.user.name}{" "}
-                  </h1>
-                  <p style={{ color: "#87878c", }}>{data.user.email }</p>
-                </div>
-              </div>
-            ) : null}
-          </div>
-        </>
-      ) : (
-        <div>Loading</div>
-      )} */
-}
